f48e462cd38000ceebf85c46a5652f56e567b893
Yamuna Ghimire <yamunaghimire959@gmail.com>
2024-01-28 18:16:52 +0545

    list

---
+++ module 2/day6/list.py
@@ -0,0 +1,28 @@
+a = [1, 2, 3, 4, 5, 'hello','hello', True]
+# pr"yamuna")
+# print(a)
+# a.pop()
+# print(a)
+# print(a.index(5))
+# print(a.index('hello',6)) #gives the index of hello 
+# # replace = a.index(4)int(a)
+# a.append()
+# # a[replace] = False
+# # print(a)
+index_of_hello = a.index('hello')
+a[index_of_hello] = False
+print(a)
+a.remove(5)
+print(a)
+print(a[3:])
+print(a[:]) #from 0th index
+print(a[::2]) #print even elements
+print(a[1::2]) #print odd elements
+print(a.count('hello'))
+sor=[1,5,6,7,8,7,66,44,33]
+sor.sort()
+print(sor)
+
+
+
+
